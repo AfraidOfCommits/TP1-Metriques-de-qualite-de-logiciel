9ec693d64a3fc7ab1f3c93b709735363616d2676
AfraidOfCommits <alexandre.lecuyer.1@umontreal.ca>
2022-09-28 22:43:49 -0400

    Added test for line count in Main.

--- src/main/java/net/frootloop/qa/metrics/Main.java
+++ src/main/java/net/frootloop/qa/metrics/Main.java
@@ -12,7 +12,7 @@ public class Main {
         SourceFileData f = JavaSourceFileParser.getParsedClassDataOf("/C:/Users/Alex/Desktop/IFT3913 - Qualité Logiciel/TP1/TP1 Metriques de qualite de logiciel/src/main/java/net/frootloop/qa/metrics/Main.java");
         System.out.println("Number of empty lines : " + f.numLinesEmpty);
         System.out.println("Number of single-lined comments : " + f.numLinesComments);
-        System.out.println("Number of other lines (code, etc.) : " + (f.numLines - f.numLinesEmpty - f.numLinesComments));
+        System.out.println("Number of other lines (code, docstring, etc.) : " + (f.numLines - f.numLinesEmpty - f.numLinesComments));
         System.out.println("In total, there are " + f.numLines + " lines in this file.");
     }
 }
