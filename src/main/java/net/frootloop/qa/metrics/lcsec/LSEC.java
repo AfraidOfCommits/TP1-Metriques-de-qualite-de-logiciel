ea2565ac30261b28c019263964a0451621b8a988
AfraidOfCommits <alexandre.lecuyer.1@umontreal.ca>
2022-09-30 22:40:28 -0400

    - Improved LSEC!

--- src/main/java/net/frootloop/qa/metrics/lcsec/LSEC.java
+++ src/main/java/net/frootloop/qa/metrics/lcsec/LSEC.java
@@ -1,22 +1,47 @@
 package net.frootloop.qa.metrics.lcsec;
 
+import net.frootloop.qa.metrics.jls.JLS;
 import net.frootloop.qa.metrics.parser.JavaSourceFileParser;
 import net.frootloop.qa.metrics.parser.result.ParsedSourceFile;
+
+import java.nio.file.Path;
 
 public class LSEC {
 
     /***
      * Etant donné un fichier source d'une classe java, calcule la métrique de taille
-     * NVLOC : nombre de lignes de code non-vides. Il doit juste sortir la valeur du NVLOC
-     * à la ligne de commandes
+     * NVLOC : nombre de lignes de code non-vides.
+     *
+     * @param filePathString : (String) chemin du fichier à aller évaluer.
      */
-    // NVLOC : nombre de lignes de code non-vides. Il doit juste sortir la valeur du NVLOC à la ligne de commandes
-    public static void printAmountOfEmptyLines(String filePath){
+    public static int getAmountOfEmptyLines(String filePathString){
+        Path path = Path.of(filePathString.replace('/', '\\').replace(":", ""));
+        return LSEC.getAmountOfEmptyLines(path);
+    }
+
+    /***
+     * Etant donné un fichier source d'une classe java, calcule la métrique de taille
+     * NVLOC : nombre de lignes de code non-vides.
+     *
+     * @param filePath : (Path) chemin du fichier à aller évaluer.
+     */
+    public static int getAmountOfEmptyLines(Path filePath){
         ParsedSourceFile parsedFile = JavaSourceFileParser.parse(filePath);
-        if(parsedFile == null)
+        if(parsedFile == null) {
             System.out.println("Le fichier donné en input à nvloc n'est pas un fichier .java : " + filePath);
-        else
-            System.out.println(parsedFile.numLinesEmpty);
+            return 0;
+        }
+        else return parsedFile.numLinesEmpty;
+    }
+
+    /***
+     * Etant donné un fichier source d'une classe java, imprime la métrique de taille
+     * NVLOC : nombre de lignes de code non-vides.
+     *
+     * @param filePathString : (String) chemin du fichier à aller évaluer.
+     */
+    public static int printAmountOfEmptyLines(String filePathString){
+       return LSEC.getAmountOfEmptyLines(filePathString);
     }
 
 }
