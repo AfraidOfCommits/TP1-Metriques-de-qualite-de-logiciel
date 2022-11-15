6465b5ab49351a48761b8552995d099897228a68
AfraidOfCommits <alexandre.lecuyer.1@umontreal.ca>
2022-11-15 12:57:51 -0500

    Added documentation for the otherwise strange ParsedClassCommitComparator class.

--- src/main/java/net/frootloop/qa/parser/util/stats/commits/ParsedClassCommitComparator.java
+++ src/main/java/net/frootloop/qa/parser/util/stats/commits/ParsedClassCommitComparator.java
@@ -6,6 +6,14 @@ import java.util.Comparator;
 
 public class ParsedClassCommitComparator implements Comparator<ParsedClass> {
 
+    /**
+     * Serves as a Comparator class in order to sort a list of ParsedClass objects
+     * by their number of commits.
+     *
+     * @param x the first ParsedClass object to be compared.
+     * @param y the second ParsedClass object to be compared.
+     * @return -1 if x < y, 0 if x == y, or 1 if x > y
+     */
     @Override
     public int compare(ParsedClass x, ParsedClass y) {
         int commitsX = x.getNumCommits();
